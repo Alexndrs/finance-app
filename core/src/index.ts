bc6919c8e0265c99cd61a5eb6263b7ed9c8495ca
Alexndrs <alexandred56700@gmail.com>
2025-04-20 16:47:52 -0400

    Electron+React+Typescript App setup w/ this tuto : https://www.youtube.com/watch?v=fP-371MN0Ck

--- core/src/index.ts
+++ core/src/index.ts
@@ -3,3 +3,5 @@ export * from './db/databaseAdapter';
 export * from './db/sqliteAdapter';
 export * from './db/mongodbAdapter';
 export * from './db/memoryAdapter';
+export * from './auth/auth'
+export * from './browseEmails/mail'
